ae9ba0fa170b74ff26899c1402f230beaecd5c1b
Mesquita02 <lmesquitas@gmail.com>
2025-11-05 14:34:31 -0300

    Finalizado

--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from datetime import datetime
+
 
 class Fotografia(models.Model):
+    
+    OPCOES_CATEGORIA = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('GALÁXIA', 'Galáxia'),
+        ('ESTRELA', 'Estrela'),
+        ('PLANETA', 'Planeta'),
+    ]        
+    
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=True, blank=True) 
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=True)
     
     def __str__(self):
-       return f"Fotografia  [nome={self.nome}]"
+       return self.nome
--- setup/settings.py
+++ setup/settings.py
@@ -113,6 +113,10 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Cofigurações de mídia
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
